a0159d3de6c3ddee576ad6df3904755f373997c0
hannahmi <hannah michal@HANNAH>
2024-06-13 19:06:26 +0300

    Documentation of Geometry findIntersection and geometries

--- unitTests/geometries/TubeTests.java
+++ unitTests/geometries/TubeTests.java
@@ -138,64 +138,242 @@ class TubeTests {
         assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
     }
 
-
+    // **** Group: Ray is orthogonal to axis and not begins against the axis head
+    // *****************
     Vector v2=new Vector(0,1,0);
+    // TC21: Ray starts outside and the line is outside (0 points)
     @Test
     void testFindIntersections11() {
-        Ray r=new Ray(new Point(3,0,0),v2);
+        Ray r=new Ray(new Point(3,0,1),v2);
         assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
     }
 
     @Test
+        // TC22: The line is tangent and the ray starts before the tube (0 points)
     void testFindIntersections12() {
-        Ray r=new Ray(new Point(2,-3,0),v2);
-        assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
-    }
+        Ray r=new Ray(new Point(2,-3,1),v2);
+       assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
 
+    }
+    // TC23: The line is tangent and the ray starts at the tube (0 points)
     @Test
     void testFindIntersections13() {
-        Ray r=new Ray(new Point(2,0,0),v2);
+        Ray r=new Ray(new Point(2,0,1),v2);
         assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
     }
-
+    // TC24: The line is tangent and the ray starts after the tube (0 points)
     @Test
     void testFindIntersections14() {
-        Ray r=new Ray(new Point(2,3,0),v2);
+        Ray r=new Ray(new Point(2,3,1),v2);
         assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
     }
-
+    // TC25: Ray starts before (2 points)6
     @Test
     void testFindIntersections15() {
-        Ray r=new Ray(new Point(1.5,-3,0),v2);
+        Ray r=new Ray(new Point(1.5,-3,1),v2);
         final var result = tube.findIntersections(r)
                 .stream().toList();
         assertEquals(2, result.size(),"ERROR:must be 2 intersections" );
     }
 
  Vector v3=new Vector(-1,0,0);
+    // TC26: Ray starts at the surface and goes inside (1 point)
     @Test
     void testFindIntersections16() {
-     Ray r=new Ray(new Point(2,0,0),v3);
+     Ray r=new Ray(new Point(2,0,1),v3);
         final var result = tube.findIntersections(r)
                 .stream().toList();
         assertEquals(1, result.size(),"ERROR:must be 1 intersections" );
 
     }
-
+    // TC27: Ray starts inside (1 point)
     @Test
     void testFindIntersections17() {
+        Ray r=new Ray(new Point(1.5,0,1),v3);
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(1, result.size(),"ERROR:must be 1 intersections" );
+    }
+
+    Vector v4=new Vector(1,0,1);
+    // TC28: Ray starts at the surface and goes outside (0 points)
+    @Test
+    void testFindIntersections18() {
+        Ray r=new Ray(new Point(2,0,1),v4);
+        assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
+    }
+    // TC29: Ray starts after
+    @Test
+    void testFindIntersections19() {
+        Ray r=new Ray(new Point(3,0,1),v4);
+        assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
+    }
+    // TC30: Ray starts before and crosses the axis (2 points)
+    @Test
+    void testFindIntersections20() {
+        Ray r=new Ray(new Point(1,3,1),new Vector(0,-1,0));
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(2, result.size(),"ERROR:must be 2 intersections" );
+    }
+    // TC31: Ray starts at the surface and goes inside and crosses the axis
+    @Test
+    void testFindIntersections21() {
+        Ray r = new Ray(new Point(1, 1, 1), new Vector(0, -1, 0));
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(1, result.size(), "ERROR:must be 1 intersections");
+    }
+    // TC32: Ray starts inside and the line crosses the axis (1 point)
+    @Test
+    void testFindIntersections22() {
+        Ray r = new Ray(new Point(1, 0.5, 1), new Vector(0, -1, 0));
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(1, result.size(), "ERROR:must be 1 intersections");
+    }
+    // TC35: Ray start at the axis
+    @Test
+    void testFindIntersections23() {
+        Ray r=new Ray(new Point(1,0,1),new Vector(0, -1, 0));
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(1, result.size(), "ERROR:must be 1 intersections");
+    }
+    // **** Group: Ray is orthogonal to axis and begins against the axis head
+    // *****************
+    // TC41: Ray starts outside and the line is outside (
+    @Test
+    void testFindIntersections24() {
+        Ray r=new Ray(new Point(3,0,0),v2);
+        assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
+    }
+    @Test
+        // TC22: The line is tangent and the ray starts before the tube (0 points)
+    void testFindIntersections25() {
+        Ray r=new Ray(new Point(2,-3,0),v2);
+        assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
+
+    }
+    // TC23: The line is tangent and the ray starts at the tube (0 points)
+    @Test
+    void testFindIntersections26() {
+        Ray r=new Ray(new Point(2,0,0),v2);
+        assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
+    }
+    // TC24: The line is tangent and the ray starts after the tube (0 points)
+    @Test
+    void testFindIntersections27() {
+        Ray r=new Ray(new Point(2,3,0),v2);
+        assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
+    }
+    // TC25: Ray starts before (2 points)6
+    @Test
+    void testFindIntersections28() {
+        Ray r=new Ray(new Point(1.5,-3,0),v2);
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(2, result.size(),"ERROR:must be 2 intersections" );
+    }
+
+    // TC26: Ray starts at the surface and goes inside (1 point)
+    @Test
+    void testFindIntersections29() {
+        Ray r=new Ray(new Point(2,0,0),v3);
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(1, result.size(),"ERROR:must be 1 intersections" );
+
+    }
+    // TC27: Ray starts inside (1 point)
+    @Test
+    void testFindIntersections30() {
         Ray r=new Ray(new Point(1.5,0,0),v3);
         final var result = tube.findIntersections(r)
                 .stream().toList();
-        assertEquals(1, result.size(),"ERROR:must be 2 intersections" );
+        assertEquals(1, result.size(),"ERROR:must be 1 intersections" );
     }
 
-    Vector v4=new Vector(1,0,0);
+    // TC28: Ray starts at the surface and goes outside (0 points)
     @Test
-    void testFindIntersections18() {
+    void testFindIntersections31() {
         Ray r=new Ray(new Point(2,0,0),v4);
         assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
     }
+    // TC29: Ray starts after
+    @Test
+    void testFindIntersections32() {
+        Ray r=new Ray(new Point(3,0,0),v4);
+        assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
+    }
+    // TC30: Ray starts before and crosses the axis (2 points)
+    @Test
+    void testFindIntersections33() {
+        Ray r=new Ray(new Point(1,3,0),new Vector(0,-1,0));
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(2, result.size(),"ERROR:must be 2 intersections" );
+    }
+    // TC31: Ray starts at the surface and goes inside and crosses the axis
+    @Test
+    void testFindIntersections34() {
+        Ray r = new Ray(new Point(1, 1, 0), new Vector(0, -1, 0));
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(1, result.size(), "ERROR:must be 1 intersections");
+    }
+    // TC32: Ray starts inside and the line crosses the axis (1 point)
+    @Test
+    void testFindIntersections35() {
+        Ray r = new Ray(new Point(1, 0.5, 0), new Vector(0, -1, 0));
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(1, result.size(), "ERROR:must be 1 intersections");
+    }
+    // TC35: Ray start at the axis head
+    @Test
+    void testFindIntersections36() {
+        Ray r=new Ray(new Point(1,0,0),new Vector(0, -1, 0));
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(1, result.size(), "ERROR:must be 1 intersections");
+    }
+    // **** Group: Ray's line is neither parallel nor orthogonal to the axis and
+    // *****************
+    // begins against axis head
+    // TC61: Ray's line is outside the tube
+    Point p1=new Point(2,-2,0);
+    @Test
+    void testFindIntersections37() {
+        Ray r=new Ray(p1,new Vector(1, 1, 1));
+        assertNull(tube.findIntersections(r),"ERROR:Ray does not intersect the tube");
+    }
+    // TC62: Ray's line crosses the tube and begins before
+    @Test
+    void testFindIntersections38() {
+        Ray r=new Ray(p1,new Vector(-1, 4, 4));
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(2, result.size(), "ERROR:must be 2 intersections");
+    }
+    // TC51: Ray starts at the surface and goes inside and goes through the axis
+    // head
+    @Test
+    void testFindIntersections39() {
+        Ray r=new Ray(new Point(1,-1,0),new Vector(0 , 2, 1));
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(1, result.size(), "ERROR:must be 1 intersections");
+    }
+    // TC55: Ray start at the axis head
+    @Test
+    void testFindIntersections40() {
+        Ray r=new Ray(new Point(1,0,0),new Vector(0 ,1,1 ));
+        final var result = tube.findIntersections(r)
+                .stream().toList();
+        assertEquals(1, result.size(), "ERROR:must be 1 intersections");
+    }
+
 
 
 
